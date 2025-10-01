b1637dc37dd1e1152915002dcd21a3b2c8670768
Yomanaka <212286687+Yomanaka@users.noreply.github.com>
2025-10-01 09:17:55 -0300

    teste

---
+++ Estudo aplicado a funções/02.js
@@ -0,0 +1,7 @@
+const itens = [
+{ description: 'pen', quantity: 1, price: 3},
+{ description: 'rule', quantity: 2, price: 5},
+{ description: 'erase', quantity: 2, price: 6}
+];
+
+const total = itens.reduce
